bed12d0236c7c48416070fe8565acce2452d4704
Kevin Ngo <me@ngokevin.com>
2016-09-22 15:56:49 -0700

    npm metadata build script (#7)
    * npm metadata build script
    
    * working on single registry and the compat policy
    
    * chaining fallback promises
    
    * finished build script
    
    * some docs
    
    * some more metadata (e.g., author, homepage, license)

--- README.md
+++ README.md
@@ -2,12 +2,54 @@
 
 > Work in progress.
 
-Curated collection of third-party A-Frame components.
+Curated collection of community A-Frame components.
 
-Distributed as a JSON file for programmatic consumption.
+## How It Works
+
+A single registry file is maintained at `registry.yml`. The format of a component looks like:
+
+```yml
+<npm package name>:
+  name: <component name as used from HTML>
+  versions:
+    <aframe major version series>:
+      version: <npm package version>
+      path: <relative path to component JS file from package root>
+```
+
+All other metadata will be fetched from npm, and the component build will be
+served via `unpkg.com`, a CDN for npm.
+
+This registry will then be processed and output to JSON files, one for each
+major version of A-Frame. These JSON files will be consumed by tools,
+libraries, and websites such as the [A-Frame
+Inspector](https://github.com/aframevr/aframe-inspector).
+
+Once A-Frame updates, all existing components will still work but are marked
+with a compatibility warning until either the component's entry in the registry
+is updated and compatibility confirmed. Here's how component compatibility is
+determined in relation to A-Frame:
+
+- If there is a component version explicitly listed for an A-Frame version, use that.
+- If there is no component version listed for an A-Frame version, use the
+  version registered for the previous A-Frame version if possible. Then set a
+  flag in order to raise compatibility warnings.
+- If a component version for an A-Frame version is explicitly set to `null`,
+  exclude it.
+
+## Updating the Registry
+
+Add or update your component in only the `registry.yml` file (do not touch
+files in `build/`). Then make a pull request!
+
+To build the output registry JSON files that correspond to each A-Frame version:
+
+```bash
+npm install
+npm run build
+```
 
 ## Guidelines
 
 - Keep the components in alphabetical order.
-- The `url` field should be a direct link to the hosted JS file.
-- The JS file should register the component (with `AFRAME.registerComponent`) with the same name as listed in the `package.json`.
+- The component build should self-register the component with `AFRAME.registerComponent`.
---
+++ scripts/build.js
@@ -0,0 +1,100 @@
+var deepExtend = require('deep-extend');
+var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+var req = require('superagent-promise')(require('superagent'), Promise);
+var urlJoin = require('url-join');
+var yaml = require('js-yaml');
+
+var CDN = 'https://unpkg.com/';
+
+// Major versions.
+var AFRAME_VERSIONS = ['0.2.0', '0.3.0'];
+
+// The output object, keyed by A-Frame versions.
+var OUTPUT = {};
+AFRAME_VERSIONS.forEach(function (version) {
+  OUTPUT[version] = {components: {}};
+});
+
+// Load registry.
+console.log('Processing registry.yml...');
+var REGISTRY = yaml.load(fs.readFileSync('registry.yml', 'utf-8'));
+
+// Fetch each component.
+var componentNpmNames = Object.keys(REGISTRY.components);
+var metadataFetched = componentNpmNames.map(function processComponent (npmName) {
+  var component = REGISTRY.components[npmName];
+
+  // Go through each A-Frame version and build component metadata for that version.
+  // Need to memoize the promises since newer versions of components may be falling back
+  // on older versions of components, which require that they have been processed.
+  var componentsProcessed = [];
+  AFRAME_VERSIONS.forEach(function pushPromise (aframeVersion, index) {
+    componentsProcessed.push(processVersion(aframeVersion, index));
+  });
+  return Promise.all(componentsProcessed);
+
+  function processVersion (aframeVersion, index) {
+    // Component marked as explicitly not compatible with this version of A-Frame.
+    if (component.versions[aframeVersion] &&
+        component.versions[aframeVersion] === null) {
+      console.log(npmName, 'marked not compatible with', aframeVersion);
+      return Promise.resolve();
+    }
+
+    // No component version registered for this version of A-Frame. Walk backwards.
+    // Cascading effect.
+    if (!component.versions[aframeVersion]) {
+      return componentsProcessed[index - 1].then(function () {
+        var oldAFrameVersion = AFRAME_VERSIONS[index - 1];
+        if (OUTPUT[oldAFrameVersion].components[npmName]) {
+          console.log(npmName, 'marked to fall back to', oldAFrameVersion, 'entry', 'for',
+                      aframeVersion);
+          var oldEntry = deepExtend({} , OUTPUT[oldAFrameVersion].components[npmName]);
+          var component = OUTPUT[aframeVersion].components[npmName] = oldEntry;
+          component.fallbackVersion = oldAFrameVersion;
+          return;
+        }
+      });
+    }
+
+    // Build URL from component version and path.
+    var componentVersion = component.versions[aframeVersion].version;
+    var packageRoot = urlJoin(CDN, npmName + '@' + componentVersion);
+    var packageJsonUrl = urlJoin(packageRoot, 'package.json');
+
+    console.log('Fetching', npmName, componentVersion, '...');
+    return req
+      .get(packageJsonUrl)
+      .then(function metadataFetchedSuccess (res) {
+        // Gather metadata.
+        var npm = res.body;
+        console.log(npmName, 'registered to use', componentVersion, 'for', aframeVersion);
+        OUTPUT[aframeVersion].components[npmName] = {
+          author: npm.author,
+          description: npm.description,
+          file: urlJoin(packageRoot, component.versions[aframeVersion].path),
+          homepage: npm.homepage,
+          license: npm.license,
+          name: component.name
+        };
+      }, function metadataFetchedError (err) {
+        console.error('Error fetching', npmName, packageJsonUrl);
+      });
+  }
+});
+
+// Write JSON file.
+Promise.all(metadataFetched).then(function writeOutput () {
+  console.log('Registry processed, writing files...');
+  Object.keys(OUTPUT).forEach(function (aframeVersion) {
+    var output = JSON.stringify(OUTPUT[aframeVersion], null, '  ');
+    outputPath = path.join('build', aframeVersion + '.json');
+    console.log('Writing', outputPath, '...');
+    fs.writeFileSync(outputPath, output);
+  });
+  console.log('Processing complete!');
+}, function error (err) {
+  console.log(err);
+});
