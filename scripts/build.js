39fedbb81fa7cc6b53987f7fbc7b11a89d96adcd
Kevin Ngo <me@ngokevin.com>
2016-09-22 15:17:09 -0700

    chaining fallback promises

--- scripts/build.js
+++ scripts/build.js
@@ -21,63 +21,76 @@ console.log('Processing registry.yml...');
 var REGISTRY = yaml.load(fs.readFileSync('registry.yml', 'utf-8'));
 
 // Fetch each component.
-var metadataFetched = Object.keys(REGISTRY.components).map(function processComponent (npmName) {
+var componentNpmNames = Object.keys(REGISTRY.components);
+var metadataFetched = componentNpmNames.map(function processComponent (npmName) {
   var component = REGISTRY.components[npmName];
 
   // Go through each A-Frame version and build component metadata for that version.
-  return Promise.all(
-    AFRAME_VERSIONS.map(function processVersion (aframeVersion, index) {
-      // Component marked as explicitly not compatible with this version of A-Frame.
-      if (component.versions[aframeVersion] &&
-          component.versions[aframeVersion] === null) {
-        console.log(npmName, 'marked not compatible with', aframeVersion);
-        return Promise.resolve();
-      }
+  // Need to memoize the promises since newer versions of components may be falling back
+  // on older versions of components, which require that they have been processed.
+  var componentsProcessed = [];
+  AFRAME_VERSIONS.forEach(function pushPromise (aframeVersion, index) {
+    componentsProcessed.push(processVersion(aframeVersion, index));
+  });
+  return Promise.all(componentsProcessed);
 
-      // No component version registered for this version of A-Frame. Walk backwards.
-      // Cascading effect.
-      if (!component.versions[aframeVersion]) {
+  function processVersion (aframeVersion, index) {
+    // Component marked as explicitly not compatible with this version of A-Frame.
+    if (component.versions[aframeVersion] &&
+        component.versions[aframeVersion] === null) {
+      console.log(npmName, 'marked not compatible with', aframeVersion);
+      return Promise.resolve();
+    }
+
+    // No component version registered for this version of A-Frame. Walk backwards.
+    // Cascading effect.
+    if (!component.versions[aframeVersion]) {
+      var oldVersionsProcessed = [];
+      for (var i = index - 1; i >= 0; i--) {
+        oldVersionsProcessed.push(componentsProcessed[i]);
+      }
+      return Promise.all(oldVersionsProcessed).then(function () {
         for (var i = index - 1; i >= 0; i--) {
           var oldAFrameVersion = AFRAME_VERSIONS[i];
           if (OUTPUT[oldAFrameVersion].components[npmName]) {
-            console.log(npmName, 'marked to fall back to', oldAFrameVersion, 'entry');
+            console.log(npmName, 'marked to fall back to', oldAFrameVersion, 'entry', 'for',
+                        aframeVersion);
             OUTPUT[aframeVersion][npmName] = OUTPUT[oldAFrameVersion][npmName];
             OUTPUT[aframeVersion][npmName].compatibilityFallback = true;
             OUTPUT[aframeVersion][npmName].registeredVersion = oldAFrameVersion;
-            break;
+            return;
           }
         }
-        return Promise.resolve();
-      }
+      });
+    }
 
-      // Build URL from component version and path.
-      var componentVersion = component.versions[aframeVersion].version;
-      var packageRoot = urlJoin(CDN, npmName + '@' + componentVersion);
-      var packageJsonUrl = urlJoin(packageRoot, 'package.json');
+    // Build URL from component version and path.
+    var componentVersion = component.versions[aframeVersion].version;
+    var packageRoot = urlJoin(CDN, npmName + '@' + componentVersion);
+    var packageJsonUrl = urlJoin(packageRoot, 'package.json');
 
-      console.log('Fetching', npmName, componentVersion, '...');
-      return req
-        .get(packageJsonUrl)
-        .then(function metadataFetchedSuccess (res) {
-          // Gather metadata.
-          var npm = res.body;
-          console.log(npmName, 'registered to use', componentVersion, 'for', aframeVersion);
-          OUTPUT[aframeVersion].components[npmName] = {
-            description: npm.description,
-            file: urlJoin(packageRoot, component.path),
-            name: component.name,
-            registeredVersion: aframeVersion
-          };
-        }, function metadataFetchedError (err) {
-          console.error('Error fetching', npmName, packageJsonUrl);
-        });
-    })
-  );
+    console.log('Fetching', npmName, componentVersion, '...');
+    return req
+      .get(packageJsonUrl)
+      .then(function metadataFetchedSuccess (res) {
+        // Gather metadata.
+        var npm = res.body;
+        console.log(npmName, 'registered to use', componentVersion, 'for', aframeVersion);
+        OUTPUT[aframeVersion].components[npmName] = {
+          description: npm.description,
+          file: urlJoin(packageRoot, component.path),
+          name: component.name,
+          registeredVersion: aframeVersion
+        };
+      }, function metadataFetchedError (err) {
+        console.error('Error fetching', npmName, packageJsonUrl);
+      });
+  }
 });
 
 // Write JSON file.
-console.log(OUTPUT);
 Promise.all(metadataFetched).then(function writeOutput () {
+  console.log(OUTPUT);
   console.log('Registry processed, writing files...');
   Object.keys(OUTPUT).forEach(function (aframeVersion) {
     var output = JSON.stringify(OUTPUT[aframeVersion], null, '  ');
