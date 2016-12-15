f5bed365f13588a56f0c9c92d700f39beb98117c
Kevin Ngo <me@ngokevin.com>
2016-12-14 22:10:11 -0800

    change placeholder image

--- scripts/config.js
+++ scripts/config.js
@@ -2,5 +2,5 @@ var deepExtend = require('deep-extend');
 
 module.exports = deepExtend({
   CDN: 'https://unpkg.com/',
-  placeholderImage: 'https://cloud.githubusercontent.com/assets/674727/19178879/5a499302-8c0c-11e6-9bc8-5e6a130cb82e.png'
+  placeholderImage: 'https://raw.githubusercontent.com/aframevr/aframe-registry/master/scripts/placeholder.svg'
 }, require('./config.local.js'));
